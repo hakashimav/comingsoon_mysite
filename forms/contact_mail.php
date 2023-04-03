87587fa9e39656076646ec88140e186471180d91
MichMav <michmav28@gmail.com>
2023-04-04 01:45:30 +0200

    fixe end

---
+++ forms/contact_mail.php
@@ -0,0 +1,60 @@
+<?php
+$toEmail = "michmav28@gmail.com";
+$mailHeaders = "From: " . $_POST["userName"] . "<". $_POST["userEmail"] .">\r\n";
+if(mail($toEmail, $_POST["subject"], $_POST["content"], $mailHeaders)) {
+print "<p class='success'>Contact Mail Sent.</p>";
+} else {
+print "<p class='Error'>Problem in Sending Mail.</p>";
+}
+?>
+<script>
+function sendContact() {
+	var valid;	
+	valid = validateContact();
+	if(valid) {
+		jQuery.ajax({
+		url: "contact_mail.php",
+		data:'userName='+$("#userName").val()+'&userEmail='+$("#userEmail").val()+'&subject='+$("#subject").val()+'&content='+$(content).val(),
+		type: "POST",
+		success:function(data){
+		$("#mail-status").html(data);
+		},
+		error:function (){}
+		});
+	}
+}
+
+function validateContact() {
+	var valid = true;	
+	$(".demoInputBox").css('background-color','');
+	$(".info").html('');
+	
+	if(!$("#userName").val()) {
+		$("#userName-info").html("(required)");
+		$("#userName").css('background-color','#FFFFDF');
+		valid = false;
+	}
+	if(!$("#userEmail").val()) {
+		$("#userEmail-info").html("(required)");
+		$("#userEmail").css('background-color','#FFFFDF');
+		valid = false;
+	}
+	if(!$("#userEmail").val().match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)) {
+		$("#userEmail-info").html("(invalid)");
+		$("#userEmail").css('background-color','#FFFFDF');
+		valid = false;
+	}
+	if(!$("#subject").val()) {
+		$("#subject-info").html("(required)");
+		$("#subject").css('background-color','#FFFFDF');
+		valid = false;
+	}
+	if(!$("#content").val()) {
+		$("#content-info").html("(required)");
+		$("#content").css('background-color','#FFFFDF');
+		valid = false;
+	}
+	
+	return valid;
+}
+</script>
